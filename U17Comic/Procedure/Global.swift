18886d7ad159438ee628c8b21c2f50fc3ec2dff0
lizubing1992@163.com <Lzb59891488090>
2019-11-22 16:11:07 +0800

    整理代码
    整理代码

--- U17Comic/Procedure/Global.swift
+++ U17Comic/Procedure/Global.swift
@@ -12,10 +12,28 @@ import Kingfisher
 import SnapKit
 import MJRefresh
 
-extension String{
-    static let sexTypeKey = "sexTypeKey"
-    static let searchHistoryKey = "searchHistory"
+//MRAK: 应用默认颜色
+extension UIColor {
+    class var background: UIColor {
+        return UIColor(r: 242, g: 242, b: 242)
+    }
+    
+    class var theme: UIColor {
+        return UIColor(r: 29, g: 221, b: 43)
+    }
 }
+
+extension String {
+    static let searchHistoryKey = "searchHistoryKey"
+    static let sexTypeKey = "sexTypeKey"
+}
+
+extension NSNotification.Name {
+    static let USexTypeDidChange = NSNotification.Name("USexTypeDidChange")
+}
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
 
 var topVC: UIViewController? {
     var resultVC: UIViewController?
@@ -24,6 +42,12 @@ var topVC: UIViewController? {
         resultVC = _topVC(resultVC?.presentedViewController)
     }
     return resultVC
+}
+
+var isIphoneX: Bool {
+    return UI_USER_INTERFACE_IDIOM() == .phone
+        && (max(UIScreen.main.bounds.height, UIScreen.main.bounds.width) == 812
+        || max(UIScreen.main.bounds.height, UIScreen.main.bounds.width) == 896)
 }
 
 private  func _topVC(_ vc: UIViewController?) -> UIViewController? {
@@ -36,12 +60,55 @@ private  func _topVC(_ vc: UIViewController?) -> UIViewController? {
     }
 }
 
-//extension UIColor{
-//    class var background: UIColor {
-//        return UIColor(red: 242, green: 242, blue: 242)
-//    }
-//    class var theme: UIColor{
-//        return UIColor(r:29, g:221, b:43)
-//    }
-//
-//}
+//MARK: print
+func uLog<T>(_ message: T, file: String = #file, function: String = #function, lineNumber: Int = #line) {
+    #if DEBUG
+        let fileName = (file as NSString).lastPathComponent
+        print("[\(fileName):funciton:\(function):line:\(lineNumber)]- \(message)")
+    #endif
+}
+
+//MARK: Kingfisher
+extension Kingfisher where Base: ImageView {
+    @discardableResult
+    public func setImage(urlString: String?, placeholder: Placeholder? = UIImage(named: "normal_placeholder_h")) -> RetrieveImageTask {
+        return setImage(with: URL(string: urlString ?? ""),
+                        placeholder: placeholder,
+                        options:[.transition(.fade(0.5))])
+    }
+}
+
+extension Kingfisher where Base: UIButton {
+    @discardableResult
+    public func setImage(urlString: String?, for state: UIControl.State, placeholder: UIImage? = UIImage(named: "normal_placeholder_h")) -> RetrieveImageTask {
+        return setImage(with: URL(string: urlString ?? ""),
+                        for: state,
+                        placeholder: placeholder,
+                        options: [.transition(.fade(0.5))])
+        
+    }
+}
+//MARK: SnapKit
+extension ConstraintView {
+    
+    var usnp: ConstraintBasicAttributesDSL {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.snp
+        } else {
+            return self.snp
+        }
+    }
+}
+
+extension UICollectionView {
+    
+    func reloadData(animation: Bool = true) {
+        if animation {
+            reloadData()
+        } else {
+            UIView .performWithoutAnimation {
+                reloadData()
+            }
+        }
+    }
+}
---
+++ U17Comic/Procedure/Home/ViewController/UHomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UHomeViewController.swift
+//  U17Comic
+//
+//  Created by nongfadai on 2019/11/22.
+//  Copyright © 2019 lizubing. All rights reserved.
+//
+
+import UIKit
+
+class UHomeViewController: UPageViewController {
+    
+}
